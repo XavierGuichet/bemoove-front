3269d3cb57344bde845fc6bb88056893c0286144
XavierGuichet <contact@xavier-guichet.fr>
2017-05-31 16:47:06 +0200

    Add parameters to constructor to create day with date and associated workout

--- src/app/models/day.ts
+++ src/app/models/day.ts
@@ -1,9 +1,9 @@
-import { Workout }          from './workout';
+import { Workout } from './workout';
 export class Day {
     public date: Date;
     public workouts: Workout[];
-    constructor(date: Date, workouts: Workout[]) {
-        this.date = date;
-        this.workouts = workouts;
+    constructor() {
+        this.date = new Date();
+        this.workouts = [];
     }
 }
