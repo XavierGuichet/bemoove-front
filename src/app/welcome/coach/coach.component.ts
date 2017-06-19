9a7283cb1a4bcf5f35f9adb145fe32ddb9a5774a
XavierGuichet <contact@xavier-guichet.fr>
2017-06-19 16:42:32 +0200

    Add a CTA on main view and use UserManagerForm

--- src/app/welcome/coach/coach.component.ts
+++ src/app/welcome/coach/coach.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Router } from '@angular/router';
-import { MdDialog, MdDialogRef } from '@angular/material';
 
-import { SportySubscribeModalComponent } from '../../modal/sporty-subscribe/sporty-subscribe-modal';
+import { UserManagerModule  } from '../user-manager/user-manager.module';
 
 import { SpaceService } from '../../_services/space.service';
 
@@ -14,18 +12,8 @@ import { SpaceService } from '../../_services/space.service';
 })
 
 export class WelcomeCoachComponent implements OnInit {
-    public name: string;
-    public mail: string;
-    public phone: string;
-    public success: boolean = false;
-
-    private headers = new Headers({'Content-Type': 'application/json'});
-    private callMeBackUrl = 'https://www.bemoove.fr/callmeback.php';
-
     constructor(
-        public dialog: MdDialog,
         private router: Router,
-        private http: Http,
         private spaceService: SpaceService
     ) {
     }
@@ -34,28 +22,4 @@ export class WelcomeCoachComponent implements OnInit {
         this.spaceService.toggleTopBar(false);
         this.spaceService.setHeaderAbove(true);
     }
-
-    public showJoinUs() {
-      let dialogRef = this.dialog.open(SportySubscribeModalComponent);
-      dialogRef.afterClosed().subscribe((result) => {
-        //   this.selectedOption = result;
-      });
-    }
-
-    public callmeback() {
-         this.http.post(this.callMeBackUrl,
-                        {   name: this.name,
-                            mail: this.mail,
-                            phone: this.phone  },
-                        this.headers).toPromise()
-                        .then((response: Response) => console.log(response))
-                        .catch(this.handleError);
-         this.success = true;
-    }
-
-    private handleError(error: any): Promise<any> {
-      console.error('An error occurred', error); // for demo purposes only
-      return Promise.reject(error.message || error);
-    }
-
 }
