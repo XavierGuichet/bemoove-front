3f9a6d0fbf268c6423d467b9d8f2eb8200f1e10a
XavierGuichet <contact@xavier-guichet.fr>
2017-10-27 10:08:12 +0200

    Add improvement
    Add a possibility to force check error
    Check error on dirty or touched control
    Add a std method to create or update an entity
    Define abstract method to use in form

--- src/app/form/bm-react-form/bm-react-form.component.ts
+++ src/app/form/bm-react-form/bm-react-form.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'bm-react-form'
 })
 
-export class BMReactFormComponent  {
+export abstract class BMReactFormComponent  {
     public formResult: any;
     public formReady: boolean = false;
     public loading: boolean = false;
@@ -26,25 +26,25 @@ export class BMReactFormComponent  {
       this.formErrors = this.recursiveCheck(form, formErrors);
     }
 
-    public recursiveCheck(form, formErrors, validationprefix = '') {
+    public recursiveCheck(form, formErrors, validationprefix = '', force = false): any {
       if (validationprefix !== '') {
         validationprefix += '.';
       }
       for (const field in formErrors) {
         if (typeof formErrors[field] === 'string') {
           const control = form.get(validationprefix + field);
-          formErrors[field] = this.checkControlError(control, validationprefix + field);
+          formErrors[field] = this.checkControlError(control, validationprefix + field, force);
         } else if (typeof this.formErrors[field] === 'object') {
           let prefix = validationprefix + field;
-          formErrors[field] = this.recursiveCheck(this.formErrors[field], prefix);
+          formErrors[field] = this.recursiveCheck(form, this.formErrors[field], prefix, force);
         }
       }
       return formErrors;
     }
 
-    public checkControlError(control, field) {
+    public checkControlError(control, field, force = false) {
       let errorMessages = '';
-      if (control && control.dirty && !control.valid) {
+      if (control && (control.dirty || control.touched || force) && !control.valid) {
         const messages = this.validationMessages[field];
         for (const key in control.errors) {
           if (control.errors.hasOwnProperty(key)) {
@@ -59,4 +59,22 @@ export class BMReactFormComponent  {
       console.error('An error occurred', error); // for demo purposes only
       return Promise.reject(error.message || error);
     }
+
+    public createOrUpdate(service: any, model: any): Promise<any> {
+        if (!model.id) {
+          return service.create(model)
+                .then( (newModel) => newModel )
+                .catch( this.handleError );
+        } else {
+          service.update(model)
+                .then( (updatedModel) => updatedModel )
+                .catch( this.handleError );
+        }
+    }
+
+    protected abstract buildForm();
+
+    protected abstract createObjectFromModel();
+
+    protected abstract createNestedEntities(model: any): Promise<any>;
 }
