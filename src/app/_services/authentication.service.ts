6c284f9a16e29ec6074326c3bb5f323814f79285
XavierGuichet <contact@xavier-guichet.fr>
2018-03-07 06:23:39 +0100

    Remove extends ApiService to better handle 401/403

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,27 +1,23 @@
 import { Injectable, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { ApiService } from './api/api.service';
-import { Router, ActivatedRoute } from '@angular/router';
 
 import { Account } from '../models/account';
 import { SpaceService } from '../_services/space.service';
 
 @Injectable()
-export class AuthenticationService extends ApiService {
+export class AuthenticationService {
+    private headers = new Headers({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    });
   private authentificationUrl = process.env.API_URL + '/login_check';
   private AccountsUrl = process.env.API_URL + '/accounts';
-  private headersspec = new Headers({
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  });
 
   constructor(
-      @Inject(MatDialog) dialog: MatDialog,
-      private http: Http,
-      private spaceService: SpaceService,
-      private router: Router) {
-    super(dialog);
+    private http: Http,
+    private spaceService: SpaceService
+  ) {
   }
 
   /*
@@ -32,16 +28,16 @@ export class AuthenticationService extends ApiService {
     return this.http.post(this.AccountsUrl, account, this.getRequestOptions())
       .toPromise()
       .then((response) => {
-          return Promise.all([response, this.login(account.email, account.password)]);
+        return Promise.all([response, this.login(account.email, account.password)]);
       })
       .then((results) => { return results[1]; })
       .catch((res) => this.handleError(res, this));
   }
 
   public login(email: string, password: string): Promise<boolean> {
-      let account = new Account();
-      account.email = email;
-      account.password = password;
+    let account = new Account();
+    account.email = email;
+    account.password = password;
     return this.http.post(this.authentificationUrl,
       account,
       this.getRequestOptions())
@@ -61,23 +57,23 @@ export class AuthenticationService extends ApiService {
   }
 
   public getResetPasswordToken(model: any): Promise<boolean> {
-      let url = process.env.API_URL + '/send_forgotten_password_token';
-      return this.http.post(url, model, this.getRequestOptions())
-        .toPromise()
-        .then((response: Response) => {
-            return true;
-        })
-        .catch((res) => this.handleError(res, this));
+    let url = process.env.API_URL + '/send_forgotten_password_token';
+    return this.http.post(url, model, this.getRequestOptions())
+      .toPromise()
+      .then((response: Response) => {
+        return true;
+      })
+      .catch((res) => this.handleError(res, this));
   }
 
   public changePassword(model: any): Promise<boolean> {
-      let url = process.env.API_URL + '/change_forgotten_password';
-      return this.http.post(url, model, this.getRequestOptions())
-        .toPromise()
-        .then((response: Response) => {
-            return true;
-        })
-        .catch((res) => this.handleError(res, this));
+    let url = process.env.API_URL + '/change_forgotten_password';
+    return this.http.post(url, model, this.getRequestOptions())
+      .toPromise()
+      .then((response: Response) => {
+        return true;
+      })
+      .catch((res) => this.handleError(res, this));
   }
 
   // remove user from local storage to log user out
@@ -86,7 +82,23 @@ export class AuthenticationService extends ApiService {
     this.spaceService.refreshSpace();
   }
 
-  private requestOptions() {
-    return new RequestOptions({ headers: this.headersspec });
+  protected getRequestOptions() {
+    let currentAccount = JSON.parse(localStorage.getItem('currentAccount'));
+    if (currentAccount && currentAccount.token) {
+      this.headers.set('Authorization', 'Bearer ' + currentAccount.token);
+    }
+    return new RequestOptions({ headers: this.headers });
+  }
+
+  protected handleError(data: any, that: any): Promise<any> {
+    let error = data.json();
+    that.loading = false;
+    if (error.code === 401) {
+      return Promise.reject('Echec de l\'identification');
+    } else {
+      console.error('An error occurred', error); // for demo purposes only
+    }
+
+    return Promise.reject(error.message || data);
   }
 }
