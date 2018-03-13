49b1a7fa011fd96caadc5807cc466539fb43d761
XavierGuichet <contact@xavier-guichet.fr>
2018-03-13 12:02:55 +0100

    Add leading zeros

--- src/app/_pipes/time.pipe.ts
+++ src/app/_pipes/time.pipe.ts
@@ -5,11 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MyTimePipe implements PipeTransform {
     public transform(value: number, format: string): string {
-        let hh = Math.floor(value / 60).toString();
-        let mm = Math.floor(value % 60).toString();
+        let hh = this.pad(2, Math.floor(value / 60).toString(), '0');
+        let mm = this.pad(2, Math.floor(value % 60).toString(), '0');
 
         format = format || 'hh:mm';
 
         return format.replace(/hh/, hh).replace(/mm/, mm);
+    }
+
+    private pad(width, string, padding) {
+        return (width <= string.length) ? string : this.pad(width, padding + string, padding)
     }
 }
