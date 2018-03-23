22b90ab2843fde69835ef2d91ea9d6ae08368f3b
XavierGuichet <contact@xavier-guichet.fr>
2018-03-23 18:14:18 +0100

    Fix Luhn Validators and use it

--- src/app/form/validators/luhn.validator.ts
+++ src/app/form/validators/luhn.validator.ts
@@ -1,11 +1,25 @@
 import { FormControl } from '@angular/forms';
 
 export function validateLuhn(c: FormControl) {
-  let EMAIL_REGEXP = /[0-9]*/;
+    const value = c.value;
+      if (value === null || (value.length !== 14) || (isNaN(value))) {
+        return false;
+      }
 
-  return EMAIL_REGEXP.test(c.value) ? null : {
-    validateEmail: {
-      valid: false
-    }
-  };
+      let sum = 0;
+      let tmp;
+      for (let pos = (value.length - 1); pos >= 0; pos--) {
+        tmp = parseInt(value.charAt(pos), 10);
+        if ((pos % 2) === 0) { // Les positions impaires : 1er, 3è, 5è, etc...
+          tmp = tmp * 2; // On le multiplie par 2
+          if (tmp > 9) {
+            tmp -= 9; // Si le résultat est supérieur à 9, on lui soustrait 9
+          }
+        }
+        sum += tmp;
+      }
+      if ((sum % 10) === 0) {
+        return null;
+      }
+      return { validateLuhn: { value } };
 }
--- src/app/partner/organization/form/invoice-notice/invoice-notice-form.component.ts
+++ src/app/partner/organization/form/invoice-notice/invoice-notice-form.component.ts
@@ -7,29 +7,7 @@ import { BusinessService } from '../../../../_services/index';
 
 import { Organization } from '../../../../models/index';
 
-function validateLuhn(c: FormControl) {
-  const value = c.value;
-  if (value === null || (value.length !== 14) || (isNaN(value))) {
-    return false;
-  }
-
-  let sum = 0;
-  let tmp;
-  for (let pos = (value.length - 1); pos >= 0; pos--) {
-    tmp = parseInt(value.charAt(pos), 10);
-    if ((pos % 2) === 0) { // Les positions impaires : 1er, 3è, 5è, etc...
-      tmp = tmp * 2; // On le multiplie par 2
-      if (tmp > 9) {
-        tmp -= 9; // Si le résultat est supérieur à 9, on lui soustrait 9
-      }
-    }
-    sum += tmp;
-  }
-  if ((sum % 10) === 0) {
-    return null;
-  }
-  return { validateLuhn: { value } };
-}
+import { validateLuhn } from '../../../../form/validators/luhn.validator';
 
 @Component({
   selector: 'invoice-notice-form',
@@ -116,7 +94,7 @@ export class InvoiceNoticeFormComponent extends BMReactFormComponent implements 
           Siret: [this.organization.siret, [
             Validators.minLength(14),
             Validators.maxLength(14),
-            // validateLuhn
+            validateLuhn
           ]
           ],
           ShareCapital: [this.organization.shareCapital, [
