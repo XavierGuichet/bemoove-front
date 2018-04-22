bdbcd6081e633de18a68919ee75fa382940043ec
XavierGuichet <contact@xavier-guichet.fr>
2018-04-22 10:11:05 +0200

    Move service call in OnInit

--- src/app/partner/coach/view/coach-view.component.ts
+++ src/app/partner/coach/view/coach-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Coach } from '../../../models/index';
@@ -10,19 +10,21 @@ import { CoachService } from '../../../_services/index';
   templateUrl: 'coach-view.component.html'
 })
 
-export class CoachViewComponent {
+export class CoachViewComponent implements OnInit {
   public coach: Coach = new Coach();
 
   constructor(
       private coachService: CoachService,
       private route: ActivatedRoute,
       private router: Router
-  ) {
-      this.route.params
-        .switchMap( (params: Params) => this.coachService.getById(+params['id']))
-        .subscribe( (coach) => {
-            this.coach = coach;
-        });
+  ) { }
+
+  public ngOnInit() {
+    this.route.params
+      .switchMap( (params: Params) => this.coachService.getById(+params['id']))
+      .subscribe( (coach) => {
+          this.coach = coach;
+      });
   }
 
   public deleteCoach() {
