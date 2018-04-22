9611da8ee2fdea7f100fb98986f375224e7706fe
XavierGuichet <contact@xavier-guichet.fr>
2018-04-22 10:13:25 +0200

    Add country selector  export

--- src/app/shared/form.module.ts
+++ src/app/shared/form.module.ts
@@ -4,20 +4,22 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BMImageInputComponent } from '../form/bm-image-input/bm-image-input.component';
+import { CountrySelectorComponent } from '../form/country-selector/country-selector.component';
 
 import { Ng2CompleterModule } from 'ng2-completer';
 import { ImageCropperComponent, CropperSettings, Bounds } from 'ng2-img-cropper';
 
 @NgModule({
   imports:      [ CommonModule, FormsModule, ReactiveFormsModule, Ng2CompleterModule ],
-  declarations: [ ImageCropperComponent, BMImageInputComponent ],
+  declarations: [ ImageCropperComponent, BMImageInputComponent, CountrySelectorComponent ],
   exports:      [
       CommonModule,
       FormsModule,
       ReactiveFormsModule,
       Ng2CompleterModule,
       ImageCropperComponent,
-      BMImageInputComponent
+      BMImageInputComponent,
+      CountrySelectorComponent
        ]
 })
 export class BmFormModule { }
